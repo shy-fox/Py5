9778bd6ed7919226d59255e586f266f28c0504f1
ֆɦɨʀօʍɨ <42133415+shy-fox@users.noreply.github.com>
2020-12-16 16:01:42 +0900

    Create Py5Vector.py

---
+++ src/Py5Vector.py
@@ -0,0 +1,52 @@
+class Py5Vector(object):
+    x: float
+    y: float
+    z: float
+    w: float
+
+    def __init__(self, x: float, y: float, z=0.0, w=0.0):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.w = w
+
+    def add(self, other: 'Py5Vector') -> 'Py5Vector':
+        new_x = self.x + other.x
+        new_y = self.y + other.y
+        new_z = self.z + other.z
+        new_w = self.w + other.w
+
+        return Py5Vector(new_x, new_y, new_z, new_w)
+
+    def sub(self, other: 'Py5Vector') -> 'Py5Vector':
+        new_x = self.x - other.x
+        new_y = self.y - other.y
+        new_z = self.z - other.z
+        new_w = self.w - other.w
+
+        return Py5Vector(new_x, new_y, new_z, new_w)
+
+    def mult(self, other: 'Py5Vector') -> 'Py5Vector':
+        new_x = self.x * other.x
+        new_y = self.y * other.y
+        new_z = self.z * other.z
+        new_w = self.w * other.w
+
+        return Py5Vector(new_x, new_y, new_z, new_w)
+
+    def div(self, other: 'Py5Vector') -> 'Py5Vector':
+        new_x = self.x / other.x
+        new_y = self.y / other.y
+        new_z = self.z / other.z
+        new_w = self.w / other.w
+
+        return Py5Vector(new_x, new_y, new_z, new_w)
+
+    def scale(self, amount: float) -> 'Py5Vector':
+        return Py5Vector(self.x * amount, self.y * amount, self.z * amount, self.w * amount)
+
+    def get(self) -> dict[str, float]:
+        return {"x": self.x, "y": self.y, "z": self.z, "w": self.w} \
+            if self.w != 0 \
+            else {"x": self.x, "y": self.y, "z": self.z} \
+            if self.z != 0 else {"x": self.x, "y": self.y}
